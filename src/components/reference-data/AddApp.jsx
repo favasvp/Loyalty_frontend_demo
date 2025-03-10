9aa529d814535b12f1bd0aa56610989152ed5b2c
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-03-10 19:40:30 +0530

    #app

---
+++ src/components/reference-data/AddApp.jsx
@@ -0,0 +1,208 @@
+import {
+  ArrowUpTrayIcon,
+  XMarkIcon,
+  PlusIcon,
+} from "@heroicons/react/24/outline";
+import React, { useEffect, useState } from "react";
+import StyledButton from "../../ui/StyledButton";
+import { useTriggerEvents } from "../../hooks/useTriggerEvents";
+import useUiStore from "../../store/ui";
+import { useAppTypes } from "../../hooks/useAppTypes";
+
+const AddApp = ({ isOpen, onClose, onSuccess, editData }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    icon: "",
+    description: "",
+    status: true,
+  });
+  const [imagePreview, setImagePreview] = useState(null);
+  const { useCreateAppType, useUpdateAppType } = useAppTypes();
+  const createMutation = useCreateAppType();
+  const updateMutation = useUpdateAppType();
+  const { addToast } = useUiStore();
+  useEffect(() => {
+    if (editData) {
+      setFormData({
+        name: editData?.data?.name,
+        icon: editData?.data?.icon,
+        description: editData?.data?.description,
+        status: editData?.data?.status,
+      });
+      setImagePreview(editData?.data?.icon);
+    }
+  }, [editData]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (editData) {
+      updateMutation.mutate(
+        {
+          id: editData?.data?._id,
+          appTypeData: formData,
+        },
+        {
+          onSuccess: (data) => {
+            addToast({
+              type: "success",
+              message: data?.message,
+            });
+            onSuccess?.();
+            onClose?.();
+          },
+          onError: (error) => {
+            addToast({
+              type: "error",
+              message: error?.response?.data?.message,
+            });
+          },
+        }
+      );
+    } else {
+      createMutation.mutate(formData, {
+        onSuccess: (data) => {
+          addToast({
+            type: "success",
+            message: data?.message,
+          });
+          onSuccess?.();
+          onClose?.();
+        },
+        onError: (error) => {
+          addToast({
+            type: "error",
+            message: error?.response?.data?.message,
+          });
+        },
+      });
+      setFormData({
+        name: "",
+        icon: "",
+        description: "",
+        tags: [],
+      });
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+
+    if (name === "icon" && files?.length > 0) {
+      const file = files[0];
+      setFormData((prev) => ({
+        ...prev,
+        icon: "https://cdn.scoreapp.com/site/uploads/2024/09/Common-issues-of-organising-events_-1024x512.png",
+      }));
+      setImagePreview(
+        "https://cdn.scoreapp.com/site/uploads/2024/09/Common-issues-of-organising-events_-1024x512.png"
+      );
+    } else {
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black/10 flex items-center justify-center z-50 mt-10">
+      <div className="bg-white rounded-lg w-full max-w-md p-6">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-xl font-semibold text-gray-900">Add App</h2>
+          <button
+            onClick={onClose}
+            className="text-gray-400 hover:text-gray-500 cursor-pointer"
+          >
+            <XMarkIcon className="w-5 h-5" />
+          </button>
+        </div>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Title
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              placeholder="Enter Name"
+              onChange={handleChange}
+              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Icon
+            </label>
+            <div className="relative w-full">
+              <input
+                type="file"
+                name="icon"
+                accept="image/*"
+                className="hidden"
+                id="file-upload"
+                onChange={handleChange}
+              />
+              <label
+                htmlFor="file-upload"
+                className="cursor-pointer w-full border border-gray-300 rounded-lg px-3 py-3 flex items-center justify-between"
+              >
+                <span className="text-sm text-gray-700">
+                  {formData.icon ? formData.icon.name : "Choose Image"}
+                </span>
+                <ArrowUpTrayIcon className="w-5 h-5 text-gray-500" />
+              </label>
+            </div>
+          </div>
+
+          {imagePreview && (
+            <div className="mt-3">
+              <p className="text-sm text-gray-700">Preview:</p>
+              <img
+                src={imagePreview}
+                alt="Preview"
+                className="mt-2 w-32 h-32 object-cover rounded-lg border border-gray-300"
+              />
+            </div>
+          )}
+
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Description
+            </label>
+            <textarea
+              name="description"
+              value={formData.description}
+              placeholder="Enter description"
+              onChange={handleChange}
+              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+              required
+            />
+          </div>
+
+          <div className="flex justify-end gap-3 mt-6">
+            <StyledButton
+              name="Cancel"
+              onClick={onClose}
+              variant="tertiary"
+              // disabled={isLoading}
+            />
+            <StyledButton
+              name="Add App"
+              type="submit"
+              variant="primary"
+              // disabled={isLoading}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddApp;
--- src/pages/reference-data/Apps.jsx
+++ src/pages/reference-data/Apps.jsx
@@ -4,15 +4,31 @@ import RefreshButton from "../../ui/RefreshButton";
 import StyledTable from "../../ui/StyledTable";
 import Loader from "../../ui/Loader";
 import { useAppTypes } from "../../hooks/useAppTypes";
+import StyledButton from "../../ui/StyledButton";
+import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import DeleteModal from "../../ui/DeleteModal";
+import useUiStore from "../../store/ui";
+import AddApp from "../../components/reference-data/AddApp";
 
 const Apps = () => {
   const [itemsPerPage, setItemsPerPage] = useState(10);
   const [totalCount, setTotalCount] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
-  const [lastUpdated, setLastUpdated] = useState("");
-  const { useGetAppTypes } = useAppTypes();
-  const { data: appTypes, isLoading, error, refetch } = useGetAppTypes();
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const[data, setData] = useState(null);
+  const { useGetAppTypes,useDeleteAppType,useGetAppTypeById } = useAppTypes();
+  const { data: triggerAppData } = useGetAppTypeById(data?.id);
 
+  const {
+    data: appTypes,
+    isLoading,
+    error,
+    refetch,
+    dataUpdatedAt,
+  } = useGetAppTypes();
+  const [addOpen, setAddOpen] = useState(false);
+const deleteMutation = useDeleteAppType();
+const{addToast} = useUiStore();
   const tableRows = useMemo(() => {
     return appTypes?.data?.map((item) => (
       <tr key={item.id} className="hover:bg-gray-50">
@@ -36,9 +52,52 @@ const Apps = () => {
             {item?.status ? "Active" : "Inactive"}
           </span>
         </td>
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+          <div className="flex items-center gap-2">
+            <button
+              className="text-slate-400 hover:text-green-700 p-1 rounded-lg hover:bg-green-50"
+              onClick={() => handleEdit(item?._id)}
+            >
+              <PencilIcon className="w-4 h-4" />
+            </button>
+            <button
+              className="text-slate-400 hover:text-red-700 p-1 rounded-lg hover:bg-red-50"
+              onClick={() => handleDeleteOpen(item?._id)}
+            >
+              <TrashIcon className="w-4 h-4" />
+            </button>
+          </div>
+        </td>
       </tr>
     ));
   }, [appTypes]);
+  const handleEdit = (id) => {
+    setData({ id });
+    setAddOpen(true);
+  };
+  const handleDeleteOpen = async (id) => {
+    setData( id );
+    setDeleteOpen(true);
+  };
+
+  const handleDelete = () => {
+    deleteMutation.mutate(data, {
+      onSuccess: (response) => {
+        addToast({
+          type: "success",
+          message: response?.data?.message,
+        });
+      },
+      onError: (error) => {
+        addToast({
+          type: "error",
+          message: error?.response?.data?.message,
+        });
+      },
+    });
+    setDeleteOpen(false);
+    setData(null);
+  };
   return (
     <>
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
@@ -48,14 +107,25 @@ const Apps = () => {
             Apps
           </h1>
           <p className="text-xs text-gray-500 mt-1">
-            Last Updated: {lastUpdated ? lastUpdated : "Fetching..."}
+            Last Updated: {new Date(dataUpdatedAt).toLocaleString()}
           </p>
         </div>
         <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4 w-full md:w-auto">
           <RefreshButton onClick={() => refetch()} isLoading={isLoading} />
+
           <StyledSearchInput
             placeholder="Search"
             className="w-full sm:w-auto"
+          />
+          <StyledButton
+            name={
+              <>
+                <span className="text-lg leading-none">+</span> Add APP
+              </>
+            }
+            onClick={() => {
+              setAddOpen(true);
+            }}
           />
         </div>
       </div>
@@ -85,6 +155,9 @@ const Apps = () => {
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Status
               </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Actions
+              </th>
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
@@ -92,6 +165,22 @@ const Apps = () => {
           </tbody>
         </StyledTable>
       )}
+        <AddApp
+        isOpen={addOpen}
+        onClose={() => {
+          setAddOpen(false);
+        }}
+        onSuccess={() => {
+          setData({});
+        }}
+        editData={triggerAppData}
+      />
+      <DeleteModal
+        data={"App"}
+        isOpen={deleteOpen}
+        onClose={() => setDeleteOpen(false)}
+        onConfirm={handleDelete}
+      />
     </>
   );
 };
